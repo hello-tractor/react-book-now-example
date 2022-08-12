0d1975aa96612586568732e0ba26e4e704e5818d
dig <digvijay.solanki@zapcg.com>
2022-08-12 13:29:14 +0530

    example project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef } from 'react';
 
 function App() {
+
+  const firstUpdate = useRef(true);
+
+  useEffect(()=>{
+    // for strict mode
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
+      window.HTSDK.init("xxxxxx-api-key-xxxxxx")
+
+      var booknowbutton = document.querySelector(".ht-book-now-button")
+
+      window.HTSDK.registerOnBookingCallbackHook(booknowbutton.id, {
+          onSuccess : ()=>{},
+          onFailure : ()=>{},
+      });
+
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a id="unqiue-botton-id" color="#ff481d" className="ht-book-now-button">
+          Book Now
+      </a>
     </div>
   );
 }
